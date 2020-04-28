bc739666b96328716f78635938738c0f7aa810e5
nejdetkadir <nejdetkadir.550@gmail.com>
2020-04-28 04:43:59 +0300

    Add one-to-many association between users and articles from back-end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 4, maximum: 20}
-                       
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false }, 
